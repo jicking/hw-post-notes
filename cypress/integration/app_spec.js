9e58de88724695bc458b1a6b928d4615d8902468
jicking <7297029+jicking@users.noreply.github.com>
2022-04-03 13:34:23 +0800

    set tests

---
+++ cypress/integration/app_spec.js
@@ -0,0 +1,45 @@
+const baseUrl = `http://localhost:3000`
+
+describe('Homepage', () => {
+  it('loads home page', () => {
+    cy.visit(baseUrl)
+  })
+  it('has posts', () => {
+    cy.visit(baseUrl)
+    cy.get('.main-container .card').contains('View Full Post')
+  })
+  it('has view full post that works ', () => {
+    cy.visit(baseUrl)
+    cy.get('.main-container .card').contains('View Full Post').click()
+    cy.url().should('include', '/post?id=')
+  })
+  it('has refresh btn', () => {
+    cy.visit(baseUrl)
+    cy.get('.main-container').contains('Refresh').click()
+    cy.get('.main-container .card').contains('View Full Post')
+  })
+
+  // TODO
+  it('has pagination that works ', () => {
+    cy.visit(baseUrl)
+    cy.get('.pagination').contains('1')
+    cy.get('.pagination').contains('2').click()
+    cy.url().should('include', '/?page=2')
+    cy.get('.pagination').contains('Previous').click()
+    cy.url().should('include', '/?page=1')
+    cy.get('.pagination').contains('Next').click()
+    cy.url().should('include', '/?page=2')
+  })
+})
+
+describe('Other tests', () => {
+  it('loads about page', () => {
+    cy.visit(`${baseUrl}/about`)
+  })
+  it('has github link that works', () => {
+    cy.visit(baseUrl)
+    cy.get('.navbar')
+      .contains('Check Code')
+      .should('have.attr', 'href', 'https://github.com/jicking/hw-post-notes')
+  })
+})
